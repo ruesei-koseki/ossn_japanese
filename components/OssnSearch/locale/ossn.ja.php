63317c106ae8da9a748312c2bbceec6717d3b676
ryusei koseki <51506011+ryusei-koseki@users.noreply.github.com>
2019-10-16 17:35:08 +0900

    Update ossn.ja.php

--- components/OssnSearch/locale/ossn.ja.php
+++ components/OssnSearch/locale/ossn.ja.php
@@ -12,8 +12,8 @@
 $ja = array(
 	'ossn:search' => '検索',
 	'result:type' => 'RESULT TYPE',
-	'search:result' => 'Search results for %s',
-	'ossn:search:topbar:search' => 'Search groups, friends and more.',
-	'ossn:search:no:result' => 'No results found!',
+	'search:result' => '%s の検索結果',
+	'ossn:search:topbar:search' => '検索グループ、友人など。',
+	'ossn:search:no:result' => '結果が見つかりません！',
 );
 ossn_register_languages('ja', $ja); 
