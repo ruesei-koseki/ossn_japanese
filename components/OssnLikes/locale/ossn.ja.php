bf6c48e16b6b2d8fcdedd1cc90c3dc4daf8950ed
ryusei koseki <51506011+ryusei-koseki@users.noreply.github.com>
2019-10-16 17:14:26 +0900

    Update ossn.ja.php

--- components/OssnLikes/locale/ossn.ja.php
+++ components/OssnLikes/locale/ossn.ja.php
@@ -8,5 +8,5 @@
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
-$ja = array('people:like:this' => 'People who liked this post',);
+$ja = array('people:like:this' => '次の方がいいねと言っています。',);
 ossn_register_languages('ja', $ja); 
